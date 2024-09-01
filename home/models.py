de21d252d2a3bf56f7d16cd9d5c4a64a8af8737c
gehsso <jepher_son@hotmail.com>
2024-09-01 15:12:18 -0300

    Mysql

--- home/models.py
+++ home/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Produto(models.Model):
-    nome = models.CharField(max_length=30)
+    nome = models.CharField(max_length=40)
+    sigla = models.CharField(max_length=40)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
 
--- pweb/settings.py
+++ pweb/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'pweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zpfvzz_django',
+        'USER': 'zpfvzz_django',
+        'PASSWORD': '25ymrmf14',
+        'HOST': 'mysql-ag-br1-19.hospedagemelastica.com.br',
+        'PORT': '3306',
     }
 }
 
